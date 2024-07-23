43f34da2417a56f6c44b84c2a696791efe53edfd
salahmohsen <salahmmohsen@gmail.com>
2024-07-23 23:42:52 +0300

    refactor(dashboard/blogs): :recycle: reset create-blog page

--- app/(dashboard)/dashboard/blog-management/create-blog/page.tsx
+++ app/(dashboard)/dashboard/blog-management/create-blog/page.tsx
@@ -1,24 +1,8 @@
 "use client";
 
-import { BlockEditor } from "@/tipTap/components/BlockEditor";
 import "@/tipTap/styles/index.css";
 import "@/app/editor.css";
-import { useBlockEditor } from "@/tipTap/hooks/useBlockEditor";
-import { initialContent } from "@/tipTap/lib/data/course.initialContent";
 
 export default function CreateBlog() {
-  const { editor, content, characterCount, leftSidebar } = useBlockEditor({
-    initialContent: initialContent,
-    defaultSidebarOpen: true,
-  });
-
-  return (
-    <BlockEditor
-      editor={editor}
-      characterCount={characterCount}
-      leftSidebar={leftSidebar}
-    >
-      Form
-    </BlockEditor>
-  );
+  return <p>Work in Progress</p>;
 }
