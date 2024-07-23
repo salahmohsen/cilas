fe31040b16c564d69a8dbc338d74ddb15e1a94c5
salahmohsen <salahmmohsen@gmail.com>
2024-07-23 23:31:03 +0300

    refactor(dashboard/courses): :label: update course schema types

--- types/course.schema.ts
+++ types/course.schema.ts
@@ -14,38 +14,19 @@ import { z } from "zod";
 
 export const courseSchema = z
   .object({
-    enTitle: optional_string,
-    arTitle: optional_string,
-    enContent: optional_string,
-    arContent: optional_string,
+    title: required_string,
+    content: required_string,
     fellowId: required_string,
     category: required_string,
-    image: optional_file,
+    featuredImage: optional_file,
     attendance: required_string,
     isRegistrationOpen: required_boolean,
-    price: optional_number,
+    suggestedPrice: z.array(z.number(), z.number()).nonempty(),
     timeSlot: required_timeSlot,
     days: optional_selectOptions,
-    courseFlowUrl: optional_url,
     applyUrl: optional_url,
     startDate: required_date,
     endDate: required_date,
-  })
-  .refine((data) => data.arTitle || data.enTitle, {
-    path: ["enTitle"],
-    message: "At least one English or Arabic title is required",
-  })
-  .refine((data) => data.enContent || data.arContent, {
-    path: ["enContent"],
-    message: "At least one English or Arabic content is required",
-  })
-  .refine((data) => data.arTitle || data.enTitle, {
-    path: ["arTitle"],
-    message: "At least one English or Arabic title is required.",
-  })
-  .refine((data) => data.enContent || data.arContent, {
-    path: ["arContent"],
-    message: "At least one English or Arabic content is required",
   })
   .refine((data) => data.endDate > data.startDate, {
     path: ["startDate"],
@@ -60,16 +41,14 @@ export type CourseSchema = z.infer<typeof courseSchema>;
 
 // Default Values for Course Form
 export const courseFormDefaultValues: CourseSchema | {} = {
-  enTitle: "",
-  enContent: "",
-  arTitle: "",
-  arContent: "",
+  title: "",
+  content: "",
   fellowId: "",
   category: "",
   image: "",
   attendance: "",
   isRegistrationOpen: "",
-  price: "",
+  suggestedPrice: [2000, 3000],
   timeSlot: {
     from: new Date(new Date().setHours(0, 0, 0, 0)),
     to: new Date(new Date().setHours(0, 0, 0, 0)),
