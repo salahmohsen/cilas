786f13f91257081c892c900a3def76d3090d347f
salahmohsen <salahmmohsen@gmail.com>
2024-07-23 23:36:41 +0300

    refactor(dashboard/courses): :recycle: extract course submit buttons to a seperate component

---
+++ components/dashboard/form/course/submit.buttons.tsx
@@ -0,0 +1,45 @@
+import { useCourseState } from "@/providers/CourseState.provider";
+import { SubmitButton } from "../inputs/input.submit";
+import { Dispatch, SetStateAction } from "react";
+
+type SubmitButtonsProps = {
+  isLoading: {
+    primaryButton: boolean;
+    secondaryButton: boolean;
+  };
+  editMode: boolean;
+  draftMode: boolean;
+  setDraftMode: Dispatch<SetStateAction<boolean>>;
+};
+
+export function SubmitButtons({
+  isLoading,
+  editMode,
+  draftMode,
+  setDraftMode,
+}: SubmitButtonsProps) {
+  const { dispatch } = useCourseState();
+  return (
+    <div className="flex gap-2">
+      <SubmitButton
+        isLoading={isLoading.secondaryButton}
+        value={editMode && !draftMode ? "Convert To Draft" : "Save Draft"}
+        variant="secondary"
+        handleOnClick={() => {
+          setDraftMode(true);
+          dispatch({ type: "SET_FILTER", payload: "draft" });
+        }}
+      />
+
+      <SubmitButton
+        variant="default"
+        isLoading={isLoading.primaryButton}
+        value={editMode && !draftMode ? "Save Changes" : "Publish Course"}
+        handleOnClick={() => {
+          setDraftMode(false);
+          dispatch({ type: "SET_FILTER", payload: "published" });
+        }}
+      />
+    </div>
+  );
+}
