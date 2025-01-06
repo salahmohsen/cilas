526321e4e8eaa9da1caca728f4b354c1ec5f50bc
salahmohsen <salahmmohsen@gmail.com>
2025-01-06 18:42:28 +0200

    feat(dashboard/courses): :sparkles: add confirmation dialog for deleting course

--- components/dashboard/course-management/courses/course.item.tsx
+++ components/dashboard/course-management/courses/course.item.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { forwardRef, useState } from "react";
 import { useCourseState } from "@/lib/providers/CourseState.provider";
+import { forwardRef, useState } from "react";
 
-import Link from "next/link";
 import { format } from "date-fns";
+import Link from "next/link";
 
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -13,11 +15,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
 
-import { Calendar, Ellipsis, User } from "lucide-react";
+import { ConfirmationDialog } from "@/components/shared/confirmation.dialog";
 import { CourseWithSafeFellow } from "@/lib/types/drizzle.types";
+import { Calendar, Ellipsis, User } from "lucide-react";
 
 type CourseItemProps = { course: CourseWithSafeFellow };
 
@@ -28,6 +29,8 @@ export const CourseItem = forwardRef<HTMLLIElement, CourseItemProps>(
       dispatch,
       handleDelete,
     } = useCourseState();
+    const [isConfirmationDialogOpen, setIsConfirmationDialogOpen] =
+      useState<boolean>(false);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleSelect = (id: number) => {
@@ -61,73 +64,89 @@ export const CourseItem = forwardRef<HTMLLIElement, CourseItemProps>(
     };
 
     return (
-      <li
-        className={`flex cursor-pointer items-center justify-between gap-5 rounded-md border px-5 py-6 text-sm font-medium transition-all duration-300 lg:group-hover/list:scale-100 lg:group-hover/list:opacity-50 lg:hover:!scale-[1.02] lg:hover:bg-accent lg:hover:!opacity-100 ${isCourseSelected?.[course.id] || isMenuOpen ? "!scale-[1.02] bg-accent !opacity-100" : "bg-transparent"}`}
-        onClick={() => handleSelect(course.id)}
-        data-course-id={course.id}
-        ref={ref}
-      >
-        <div className="flex flex-col gap-4">
-          <span className="flex gap-1 text-xs font-light">
-            <User size={16} strokeWidth={1.5} />
-            {`${course.fellow?.firstName} ${course.fellow?.lastName}`}
-          </span>
-          <div className="flex flex-col gap-2 md:flex-row md:items-start md:gap-2">
-            <Badge
-              variant="default"
-              className="h-6 min-w-max max-w-max rounded-sm"
-            >
-              {courseStatues()}
-            </Badge>
-            <p className="line-clamp-3 leading-relaxed lg:line-clamp-1">
-              {course.enTitle || course.arTitle}
-            </p>
+      <>
+        <li
+          className={`flex cursor-pointer items-center justify-between gap-5 rounded-md border px-5 py-6 text-sm font-medium transition-all duration-300 lg:group-hover/list:scale-100 lg:group-hover/list:opacity-50 lg:hover:!scale-[1.02] lg:hover:bg-accent lg:hover:!opacity-100 ${isCourseSelected?.[course.id] || isMenuOpen ? "!scale-[1.02] bg-accent !opacity-100" : "bg-transparent"}`}
+          onClick={() => handleSelect(course.id)}
+          data-course-id={course.id}
+          ref={ref}
+        >
+          <div className="flex flex-col gap-4">
+            <span className="flex gap-1 text-xs font-light">
+              <User size={16} strokeWidth={1.5} />
+              {`${course.fellow?.firstName} ${course.fellow?.lastName}`}
+            </span>
+            <div className="flex flex-col gap-2 md:flex-row md:items-start md:gap-2">
+              <Badge
+                variant="default"
+                className="h-6 min-w-max max-w-max rounded-sm"
+              >
+                {courseStatues()}
+              </Badge>
+              <p className="line-clamp-3 leading-relaxed lg:line-clamp-1">
+                {course.enTitle || course.arTitle}
+              </p>
+            </div>
+            <span className="flex gap-1 text-xs font-light">
+              <Calendar size={16} strokeWidth={1.5} />
+              {format(course.startDate, "MMMM dd yyyy")}
+            </span>
           </div>
-          <span className="flex gap-1 text-xs font-light">
-            <Calendar size={16} strokeWidth={1.5} />
-            {format(course.startDate, "MMMM dd yyyy")}
-          </span>
-        </div>
-        <div>
-          <DropdownMenu onOpenChange={setIsMenuOpen}>
-            <DropdownMenuTrigger asChild>
-              <Button
-                size="icon"
-                variant="outline"
-                className={`h-8 w-8 ${isCourseSelected ? "bg-background text-foreground" : ""}`}
-              >
-                <Ellipsis className="h-3.5 w-3.5" />
-                <span className="sr-only">More</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <Link
-                href={`/dashboard/course-management/edit-course?id=${course.id}`}
-              >
-                <DropdownMenuItem onClick={(e) => e.stopPropagation()}>
-                  Edit
-                </DropdownMenuItem>
-              </Link>
-              <Link
-                href={`/dashboard/course-management/create-course?duplicate-course=${course.id}`}
-              >
-                <DropdownMenuItem onClick={(e) => e.stopPropagation()}>
-                  Duplicate
-                </DropdownMenuItem>
-              </Link>
-              <DropdownMenuSeparator />
+          <div>
+            <DropdownMenu onOpenChange={setIsMenuOpen}>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  size="icon"
+                  variant="outline"
+                  className={`h-8 w-8 ${isCourseSelected ? "bg-background text-foreground" : ""}`}
+                >
+                  <Ellipsis className="h-3.5 w-3.5" />
+                  <span className="sr-only">More</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <Link
+                  href={`/dashboard/course-management/edit-course?id=${course.id}`}
+                >
+                  <DropdownMenuItem
+                    onClick={(e) => e.stopPropagation()}
+                    className="cursor-pointer"
+                  >
+                    Edit
+                  </DropdownMenuItem>
+                </Link>
+                <Link
+                  href={`/dashboard/course-management/create-course?duplicate-course=${course.id}`}
+                >
+                  <DropdownMenuItem
+                    onClick={(e) => e.stopPropagation()}
+                    className="cursor-pointer"
+                  >
+                    Duplicate
+                  </DropdownMenuItem>
+                </Link>
+                <DropdownMenuSeparator />
 
-              <DropdownMenuItem
-                className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
-                onSelect={() => handleDelete(course.id)}
-                onClick={(e) => e.stopPropagation()}
-              >
-                Delete
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-      </li>
+                <DropdownMenuItem
+                  className="cursor-pointer bg-destructive text-destructive-foreground hover:bg-destructive/90"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setIsConfirmationDialogOpen(true);
+                  }}
+                >
+                  Delete
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </li>
+        <ConfirmationDialog
+          isOpen={isConfirmationDialogOpen}
+          setIsOpen={setIsConfirmationDialogOpen}
+          title={`Delete ${(course.enTitle || course.arTitle) ?? "Course"}`}
+          onConfirm={() => handleDelete(course.id)}
+        />
+      </>
     );
   },
 );
---
+++ components/shared/confirmation.dialog.tsx
@@ -0,0 +1,47 @@
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+
+type ConfirmationDialogProps = {
+  title: string;
+  message?: string;
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  onConfirm: () => void;
+};
+
+export const ConfirmationDialog = ({
+  title,
+  message,
+  onConfirm,
+  isOpen,
+  setIsOpen,
+}: ConfirmationDialogProps) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>
+          {message ||
+            "This action cannot be undone. Are you sure you want to proceed?"}
+        </DialogDescription>
+        <DialogFooter>
+          <Button variant="secondary" onClick={() => setIsOpen(false)}>
+            Cancel
+          </Button>
+          <Button onClick={onConfirm} variant={"destructive"}>
+            Proceed
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
